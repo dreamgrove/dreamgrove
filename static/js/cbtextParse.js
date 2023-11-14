82fcdc057d7aca4f0ce4cdd0eaeb33b85ea96f1b
thevinter <contact@thevinter.com>
2023-11-14 21:18:59 +0100

    feat: New Checkboxes Functionality

--- static/js/cbtextParse.js
+++ static/js/cbtextParse.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.cbtext-placeholder').forEach(function(el) {
-        var id = el.getAttribute('data-id');
+        var id = el.getAttribute('data-tag');
         var content = el.getAttribute('data-content');
         var replacementHtml = '';
 
         // Check if the placeholder is the only child of a list item
         if (el.parentNode.tagName === 'LI' && el.parentNode.childNodes.length === 1) {
-            replacementHtml = '<ul data-tag="' + id + '"><li>' + content + '</li></ul>';
+            replacementHtml = '<ul data-tag="' + id + '">' + content + '</ul>';
         } else if (el.parentNode.tagName === 'LI') {
             // Inline within a list item
             replacementHtml = '<span data-tag="' + id + '">' + content + '</span>';
