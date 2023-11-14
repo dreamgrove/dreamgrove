80bd89ffd93179a1327741f797eb2b75b52e768c
Nikita Brancatisano <33670112+thevinter@users.noreply.github.com>
2023-11-14 18:49:17 +0100

    feat: Creates cbtextParse.js

---
+++ static/js/cbtextParse.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.cbtext-placeholder').forEach(function(el) {
+        var id = el.getAttribute('data-id');
+        var content = el.getAttribute('data-content');
+        var replacementHtml = '';
+
+        // Check if the placeholder is the only child of a list item
+        if (el.parentNode.tagName === 'LI' && el.parentNode.childNodes.length === 1) {
+            replacementHtml = '<ul data-tag="' + id + '"><li>' + content + '</li></ul>';
+        } else if (el.parentNode.tagName === 'LI') {
+            // Inline within a list item
+            replacementHtml = '<span data-tag="' + id + '">' + content + '</span>';
+        } else {
+            // Default case
+            replacementHtml = '<span data-tag="' + id + '">' + content + '</span>';
+        }
+
+        // Replace the placeholder with the appropriate HTML
+        el.outerHTML = replacementHtml;
+    });
+});
