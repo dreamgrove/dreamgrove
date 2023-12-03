e87c84ee3d586d328f71a21cc07c36a71f5d00cb
Dsune0 <106332935+Dsune0@users.noreply.github.com>
2023-12-03 15:34:26 +0100

    Reverse changes

--- static/js/checkboxToggle.js
+++ static/js/checkboxToggle.js
@@ -4,10 +4,8 @@ function toggleCheckbox(checkboxId, isChecked) {
         console.error('Checkbox with data-id "' + checkboxId + '" not found.');
         return;
     }
-
     // Set the checkbox state if 'isChecked' is provided, otherwise toggle it.
     checkbox.checked = isChecked !== undefined ? isChecked : !checkbox.checked;
-
     // If this is a radio checkbox, uncheck others in the same group.
     if (checkbox.checked && checkbox.dataset.radio) {
         const radioGroupName = checkbox.dataset.radio;
@@ -28,14 +26,7 @@ function toggleCheckbox(checkboxId, isChecked) {
     // Find all elements with the matching data-tag and toggle their visibility.
     document.querySelectorAll(`[data-tag="${checkboxId}"]`).forEach(el => {
         const listItem = el.closest('li');
-        // Get the data-negate attribute from the span itself
-        const isNegated = el.getAttribute('data-negate') === 'true';
-
-        if (isNegated) {
-            listItem.style.display = checkbox.checked ? 'none' : 'list-item';
-        } else {
-            listItem.style.display = checkbox.checked ? 'list-item' : 'none';
-        }
+        listItem.style.display = checkbox.checked ? 'list-item' : 'none';
     });
 
     // Dispatch the change event if called from an event listener.
@@ -43,17 +34,14 @@ function toggleCheckbox(checkboxId, isChecked) {
         checkbox.dispatchEvent(new Event('change'));
     }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
     // Attach event listeners to checkboxes and set initial state
     document.querySelectorAll('input[type="checkbox"][data-id]').forEach(checkbox => {
         const checkboxId = checkbox.getAttribute('data-id');
-
         // Attach a change event listener
         checkbox.addEventListener('change', function () {
             toggleCheckbox(checkboxId);
         });
-
         // Set the initial state without dispatching the change event
         toggleCheckbox(checkboxId, checkbox.checked);
     });
