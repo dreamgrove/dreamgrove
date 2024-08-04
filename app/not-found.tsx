6017fdbc902573a209d70fdb13eb196819384ea1
thevinter <contact@thevinter.com>
2024-08-04 07:08:12 +0200

    feat: random quotes on 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,11 @@
 import Link from '@/components/Link'
 
+import data from '../other/quotes.json'
+
 export default function NotFound() {
+  const quotes = data.quotes
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
   return (
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pb-8 pt-6 md:space-y-5">
@@ -12,7 +17,7 @@ export default function NotFound() {
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
           Sorry we couldn't find this page.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">"{randomQuote}"</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
