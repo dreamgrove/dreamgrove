77153073d817cd6901c6aeb9cf0e957fa618a827
thevinter <contact@thevinter.com>
2024-08-29 23:22:28 +0200

    fix: Temporarily Removed HeroTalentsHeader

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -8,7 +8,6 @@ import TableWrapper from './TableWrapper'
 import Wowhead from './custom/Wowhead'
 import Talents from './custom/Talents'
 import Checkbox from './custom/Checkbox'
-import HeroTalentsHeader from './custom/HeroTalents/HeroTalentsHeader'
 import BossCard from './custom/Dungeons/BossCard'
 import Npc from './custom/Npc'
 import CheckboxProvider from './custom/CheckboxProvider'
@@ -56,7 +55,6 @@ export const components: MDXComponents = {
   Wowhead,
   Talents,
   Checkbox,
-  HeroTalentsHeader,
   BossCard,
   Npc,
   CheckboxProvider,
