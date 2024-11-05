10f40603176e843b673db5d0883498a556a35e84
thevinter <contact@thevinter.com>
2024-11-06 00:30:12 +0100

    removed other locales

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import Negotiator from 'negotiator'
 import { match } from '@formatjs/intl-localematcher'
 
-const locales = ['en-US', 'fr', 'nl-NL', 'ko-KR']
+const locales = ['en-US', 'ko-KR']
 const defaultLocale = 'en-US'
 
 function getLocale(request: NextRequest) {
