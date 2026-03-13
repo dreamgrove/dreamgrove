0f86b0009dfd33d6eb4c73fd56c4ca6760399710
thevinter <contact@thevinter.com>
2026-03-13 12:49:31 +0100

    increased image caching TTL

--- next.config.js
+++ next.config.js
@@ -77,7 +77,7 @@ module.exports = () => {
       formats: ['image/webp'],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      minimumCacheTTL: 60,
+      minimumCacheTTL: 2592000,
       qualities: [75, 100],
     },
     env: {
