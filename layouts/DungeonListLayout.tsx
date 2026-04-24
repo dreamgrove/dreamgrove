a0e8bb45f16d4ed939350c1e425af987817fdaa8
thevinter <contact@thevinter.com>
2026-04-24 16:37:38 +0200

    [Dungeons/Raids] align cards with homepage style, drop page header

--- layouts/DungeonListLayout.tsx
+++ layouts/DungeonListLayout.tsx
@@ -22,17 +22,6 @@ export default function DungeonListLayout({
 
   return (
     <div className="px-3 sm:px-6 md:px-6 lg:px-6 xl:px-3">
-      <header className="flex flex-col items-center pt-6 pb-10 text-center">
-        <h1 className="instance-index__title text-4xl text-gray-900 sm:text-5xl md:text-6xl dark:text-gray-100">
-          {title}
-        </h1>
-        <div className="instance-index__rule mt-6">
-          <span className="line" />
-          <span className="diamond" />
-          <span className="line" />
-        </div>
-      </header>
-
       {!posts.length && (
         <div className="py-16 text-center text-gray-500 dark:text-gray-400">No posts found.</div>
       )}
